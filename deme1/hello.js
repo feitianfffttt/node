61b246fef33877a7bcade9a3316508e9cd4c48ff
飞天 <189710436@qq.com>
2020-08-07 16:59:00 +0800

    xiao wen ti

--- deme1/hello.js
+++ deme1/hello.js
@@ -40,12 +40,15 @@ app.get('/ab*cd', function(req, res) {
    res.send('正则匹配');
 })
  
- 
-var server = app.listen(8081, function () {
+
+
+var server = app.listen(9090, function () {
  
   var host = server.address().address
   var port = server.address().port
   console.log(server.address)
-  console.log("应用实例，访问地址为 http://%s:%s", host, port)
+  console.log('应用实例，访问地址为 http://'+host+':'+port)
+  
  
 })
+
